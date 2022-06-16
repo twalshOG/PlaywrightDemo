2d822c19591662f53822c3245483c8ade0d1cc6b
Troy Walsh <106253115+twalshOG@users.noreply.github.com>
2022-06-16 12:01:49 -0500

    Fix dup

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -92,34 +92,6 @@ test.describe('New Todo', () => {
     await page.locator('.new-todo-BAD').fill(TODO_ITEMS[0]);
   });
 
-  
-
-
-
-
-  test('should allow me to add todo items', async ({ page }) => {
-    // Create 1st todo.
-    await page.locator('.new-todo').fill(TODO_ITEMS[0]);
-    await page.locator('.new-todo').press('Enter');
-
-    // Make sure the list only has one todo item.
-    await expect(page.locator('.view label')).toHaveText([
-      TODO_ITEMS[0]
-    ]);
-
-    // Create 2nd todo.
-    await page.locator('.new-todo').fill(TODO_ITEMS[1]);
-    await page.locator('.new-todo').press('Enter');
-
-    // Make sure the list now has two todo items.
-    await expect(page.locator('.view label')).toHaveText([
-      TODO_ITEMS[0],
-      TODO_ITEMS[1]
-    ]);
-
-    await checkNumberOfTodosInLocalStorage(page, 2);
-  });
-
   test('should clear text input field when an item is added', async ({ page }) => {
     // Create one todo item.
     await page.locator('.new-todo').fill(TODO_ITEMS[0]);
